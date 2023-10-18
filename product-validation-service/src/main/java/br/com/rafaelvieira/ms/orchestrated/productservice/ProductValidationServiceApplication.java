d83e2be23cc02c03963d54918c422074d94c2e87
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2023-10-17 23:09:20 -0300

    ms-order-sales-pattern-saga: iniciado projeto, aplicando estrutura e definiando configuracoes e informacoes

---
+++ product-validation-service/src/main/java/br/com/rafaelvieira/ms/orchestrated/productservice/ProductValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelvieira.ms.orchestrated.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationServiceApplication.class, args);
+	}
+
+}
