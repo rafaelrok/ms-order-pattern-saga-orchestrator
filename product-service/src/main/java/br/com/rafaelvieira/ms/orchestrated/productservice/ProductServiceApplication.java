0bc0609af99e68ac0e715dd9bb6ef3ec54cb5820
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2023-10-18 16:20:15 -0300

    ms-order-sales-pattern-saga: fix all projects by gradle version 7.5

--- product-service/src/main/java/br/com/rafaelvieira/ms/orchestrated/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/br/com/rafaelvieira/ms/orchestrated/productservice/ProductServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductValidationServiceApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductValidationServiceApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
--- product-service/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.product (id, code) VALUES (1, 'COMIC_BOOKS');
-INSERT INTO public.product (id, code) VALUES (2, 'BOOKS');
-INSERT INTO public.product (id, code) VALUES (3, 'MOVIES');
-INSERT INTO public.product (id, code) VALUES (4, 'MUSIC');
