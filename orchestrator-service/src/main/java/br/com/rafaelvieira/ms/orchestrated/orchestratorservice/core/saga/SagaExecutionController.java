9434983c63b7e6909f6b0af42cc95ffd81bfe06c
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2023-10-22 22:57:50 -0300

    ms-order-sales-pattern-saga: Implemented inventory and orchestrator, consumers with archetecture

---
+++ orchestrator-service/src/main/java/br/com/rafaelvieira/ms/orchestrated/orchestratorservice/core/saga/SagaExecutionController.java
@@ -0,0 +1,64 @@
+package br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.saga;
+
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.config.exception.ValidationException;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.dto.Event;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.enums.ETopics;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+import static br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.saga.SagaHandler.*;
+import static java.lang.String.format;
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class SagaExecutionController {
+
+    private static final String SAGA_LOG_ID = "ORDER ID: %s | TRANSACTION ID %s | EVENT ID %s";
+
+    public ETopics getNextTopic(Event event) {
+        if (isEmpty(event.getSource()) || isEmpty(event.getStatus())) {
+            throw new ValidationException("Source and status must be informed.");
+        }
+        var topic = findTopicBySourceAndStatus(event);
+        logCurrentSaga(event, topic);
+        return topic;
+    }
+
+    private ETopics findTopicBySourceAndStatus(Event event) {
+        return (ETopics) (Arrays.stream(SAGA_HANDLER)
+                .filter(row -> isEventSourceAndStatusValid(event, row))
+                .map(i -> i[TOPIC_INDEX])
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Topic not found!")));
+    }
+
+    private boolean isEventSourceAndStatusValid(Event event,
+                                                Object[] row) {
+        var source = row[EVENT_SOURCE_INDEX];
+        var status = row[SAGA_STATUS_INDEX];
+        return source.equals(event.getSource()) && status.equals(event.getStatus());
+    }
+
+    private void logCurrentSaga(Event event, ETopics topic) {
+        var sagaId = createSagaId(event);
+        var source = event.getSource();
+        switch (event.getStatus()) {
+            case SUCCESS -> log.info("### CURRENT SAGA: {} | SUCCESS | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+            case ROLLBACK_PENDING -> log.info("### CURRENT SAGA: {} | SENDING TO ROLLBACK CURRENT SERVICE | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+            case FAIL -> log.info("### CURRENT SAGA: {} | SENDING TO ROLLBACK PREVIOUS SERVICE | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+        }
+    }
+
+    private String createSagaId(Event event) {
+        return format(SAGA_LOG_ID,
+                event.getPayload().getId(), event.getTransactionId(), event.getId());
+    }
+}
---
+++ orchestrator-service/src/main/java/br/com/rafaelvieira/ms/orchestrated/orchestratorservice/core/service/OrchestrationService.java
@@ -0,0 +1,81 @@
+package br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.service;
+
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.dto.Event;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.dto.History;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.enums.ETopics;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.producer.SagaOrchestratorProducer;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.saga.SagaExecutionController;
+import br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.enums.EEventSource.ORCHESTRATOR;
+import static br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.enums.ESagaStatus.FAIL;
+import static br.com.rafaelvieira.ms.orchestrated.orchestratorservice.core.enums.ESagaStatus.SUCCESS;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class OrchestrationService {
+
+    private final SagaOrchestratorProducer producer;
+    private final JsonUtil jsonUtil;
+    private final SagaExecutionController sagaExecutionController;
+
+    public void startSaga(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(SUCCESS);
+        var topic = getTopic(event);
+        log.info("SAGA STARTED!");
+        addHistory(event, "Saga started!");
+        sendToProducerWithTopic(event, topic);
+    }
+
+    public void finishSagaSuccess(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(SUCCESS);
+        log.info("SAGA FINISHED SUCCESSFULLY FOR EVENT {}!", event.getId());
+        addHistory(event, "Saga finished successfully!");
+        notifyFinishedSaga(event);
+    }
+
+    public void finishSagaFail(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(FAIL);
+        log.info("SAGA FINISHED WITH ERRORS FOR EVENT {}!", event.getId());
+        addHistory(event, "Saga finished with errors!");
+        notifyFinishedSaga(event);
+    }
+
+    public void continueSaga(Event event) {
+        var topic = getTopic(event);
+        log.info("SAGA CONTINUING FOR EVENT {}", event.getId());
+        sendToProducerWithTopic(event, topic);
+    }
+
+    private ETopics getTopic(Event event) {
+        return sagaExecutionController.getNextTopic(event);
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
+    }
+
+    private void sendToProducerWithTopic(Event event, ETopics topic) {
+        producer.sendEvent(jsonUtil.toJson(event), topic.getTopic());
+    }
+
+    private void notifyFinishedSaga(Event event) {
+        producer.sendEvent(jsonUtil.toJson(event), ETopics.NOTIFY_ENDING.getTopic());
+    }
+}
